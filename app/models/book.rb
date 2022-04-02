1c2145f959725a0aa81b83284c0c7063b6a31d0b
reszcb740 <reszcb740@Gmail.com>
2022-04-02 10:49:30 +0000

    [Finish]バリデーション[Add]views,books

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,17 @@
 class Book < ApplicationRecord
-  has_many :user
-  validates :title,presence:true
-  validates :body,presence:true,length:{maximum:200}
+
+  has_one_attached :profile_image
+  belongs_to :user
+
+  validates :title, presence: true
+  validates :body, presence: true, length: { maximum: 200 }
+  validates :user_id, presence: true
+
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+      profile_image.variant(resize_to_limit: [width, height]).processed
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  belongs_to :books
   has_one_attached :profile_image
+  has_many :books, dependent: :destroy
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
 
-  
-  
-  def get_profile_image
-    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/sample-author1.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
   end
 end
