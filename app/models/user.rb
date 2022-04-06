9380bc3a51e6c3fa5c7cdc054eb88493c392767e
reszcb740 <reszcb740@Gmail.com>
2022-04-06 08:18:48 +0000

    [Add]フォロー、フォロワー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,8 @@ class User < ApplicationRecord
           has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
           has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
 
-          has_many :followings, through: :relationships, source: :follwed
-          has_many :follwers, through: :reverse_of_relationships, source: :follower
+          has_many :followings, through: :relationships, source: :followed
+          has_many :followers, through: :reverse_of_relationships, source: :follower
 
           validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
           validates :introduction, length: { maximum: 50 }
