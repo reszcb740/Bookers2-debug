d7fa0ae16e6ff4abb4db77d923df0aa09120bf8c
reszcb740 <reszcb740@Gmail.com>
2022-04-06 02:18:52 +0000

    [Finish]relationship, model, controller, route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resource :book_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get "relationships/followings" => "relationships#followings", as: "followings"
+    get "relationships/followers" => "relationships#followers", as: "followers"
+  end
   get "home/about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
