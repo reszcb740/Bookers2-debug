f70fc5c5fc0586039268466d073bdd34bd464796
reszcb740 <reszcb740@Gmail.com>
2022-04-07 07:05:29 +0000

    [Add]検索機能

--- app/models/book.rb
+++ app/models/book.rb
@@ -19,4 +19,18 @@ class Book < ApplicationRecord
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
+
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @book = Book.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @book = Book.where("name LIKE?", "#{word}")
+    elsif search == "backward_match"
+      @book = Book.where("name LIKE?", "#{word}")
+    elsif search == "pertial_match"
+      @book = Book.where("name LIKE?", "#{word}")
+    else
+      @book = Book.all
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
     get "relationships/followings" => "relationships#followings", as: "followings"
     get "relationships/followers" => "relationships#followers", as: "followers"
   end
+  get "search" => "searches#search"
   get "home/about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
