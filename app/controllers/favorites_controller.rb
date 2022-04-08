f3d782398462b0055eebc50663e0e6d103ea831e
reszcb740 <reszcb740@Gmail.com>
2022-04-08 10:53:20 +0000

    [Add]いいね非同期通信実装中

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,13 +4,13 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-    
+
   end
 
   def destroy
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    
+
   end
 end
