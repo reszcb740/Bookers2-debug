83031d3ca82aedc455d4993ca5ff7da06793e942
reszcb740 <reszcb740@Gmail.com>
2022-06-16 10:44:02 +0000

    Add books/star

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,7 +12,8 @@ import "popper.js";
 import "bootstrap";
 import "../stylesheets/application"
 import '@fortawesome/fontawesome-free/js/all'
-
+window.$ = window.jQuery = require('jquery');
+require('packs/raty')
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
